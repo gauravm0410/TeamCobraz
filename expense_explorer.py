f0d936afaad6a96fb50df11114dde1a015566e13
arnavskale18 <arnavskale18@gmail.com>
2024-09-20 23:20:36 +0530

    Update expense_explorer.py

--- expense_explorer.py
+++ expense_explorer.py
@@ -1,30 +1,33 @@
 import mysql.connector
+from datetime import date
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="Arn@Ana@2897"
 )
+monthly_budget=float(input("Enter your monthly budge: "))
 mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS expenseexplorer")
 mycursor.execute("USE expenseexplorer")
-mycursor.execute("CREATE TABLE IF NOT EXISTS transactiontable(transid INT AUTO_INCREMENT PRIMARY KEY, category VARCHAR(255), amt DOUBLE(10,2), item VARCHAR(255))")
-#list=[]
+mycursor.execute("CREATE TABLE IF NOT EXISTS transactiontable(transid INT AUTO_INCREMENT PRIMARY KEY, category VARCHAR(255), amt DOUBLE(10,2), item VARCHAR(255), trans_date DATE)")
 tup=()
 l1=[]
 while True:
     cat=input("Enter category of expenditure: ")
     amount=float(input("Enter cost of purchase: "))
     item=input("Enter item name: ")
+    current_date=date.today()
     l1.append(cat)
     l1.append(amount)
     l1.append(item)
+    l1.append(current_date)
     tup=tuple(l1)
-    sql="INSERT INTO transactiontable(category, amt, item) VALUES (%s, %s, %s)"
+    sql="INSERT INTO transactiontable(category, amt, item, transdate) VALUES (%s, %s, %s, %s)"
     mycursor.execute(sql, tup)
     mydb.commit()
     y=input("Add another transaction?(y/n): ")
     if y=='n':
         break
-mycursor.execute("SELECT * from transactiontable")
-data=mycursor.fetchall()
-data2=list(data)
+mycursor.execute("SELECT UNIQUE CATEGORIES from transactiontable")
+categories_data=mycursor.fetchall()
+categories_list=list(categories_data)
